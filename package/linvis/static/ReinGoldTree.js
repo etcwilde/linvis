cf4e43bd7102e2d013799930a23ef18ebde30599
Evan Wilde <etcwilde@uvic.ca>
2017-05-17 11:56:56 -0700

    Updates to the Reingold Tree
    Changes the interface with the Reingold Tilford Tree.
    This makes the implementation a little simpler.

--- package/linvis/static/ReinGoldTree.js
+++ package/linvis/static/ReinGoldTree.js
@@ -1,20 +1,12 @@
-function ReingoldTree(data, treeRoot) {
-    this.data = data;
-    this.height = 480;
-    this.width = 640;
-    this.root = treeRoot;
-    this.focus = treeRoot;
-    this.duration = 750;
+function BuildReingoldTree(data, treeRoot, pane, width, height, clickFunction, hoverFunction) {
 
+    var tree = d3.layout.tree().size([width, height])
+        nodes = tree.nodes(data)
+        links = tree.links(nodes);
 
-    this.tree = d3.layout.tree();
-    this.nodes = this.tree.nodes(data);
-    this.links = this.tree.links(this.nodes);
-
-
-    var max_width = 0;
-    var levelWidth = [];
-    var focus;
+    var max_width = 0
+        levelWidth = []
+        focus;
 
     readTree(data, function(el) {
         if (el.cid == cid) focus = el;
@@ -22,26 +14,12 @@ function ReingoldTree(data, treeRoot) {
         if (el.children.length > max_width) max_width = el.children.length;
         while (el.depth >= levelWidth.length) levelWidth.push(0);
         levelWidth[el.depth] += el.children.length;
-        });
+    });
 
-    this.max_width = max_width;
-    this.levelWidth = levelWidth;
-
-
-    this.radius = 15;
-    this.tree.size([d3.max(levelWidth) * this.radius * 4 + Math.pow((levelWidth.length - levelWidth.indexOf(d3.max(levelWidth))) * 2, 2),
-        levelWidth.length * this.radius * 5 + d3.max(levelWidth)]);
-    this.nodes = this.tree.nodes(data);
-    this.links = this.tree.links(this.nodes);
-}
-
-
-ReingoldTree.prototype.draw = function(pane, width, height, clickFunction, hoverFunction) {
     var color = d3.scale.linear()
-        .domain([1, this.max_width])
+        .domain([1, max_width])
         .range(["hsl(212,100%,75%)", "hsl(212,100%,25%)"])
         .interpolate(d3.interpolateHcl);
-
 
     function zoom() {
         svgGroup.attr("transform", "translate("+d3.event.translate+")scale("+d3.event.scale+")");
@@ -72,7 +50,6 @@ ReingoldTree.prototype.draw = function(pane, width, height, clickFunction, hover
     var diagonal = d3.svg.diagonal()
         .projection(function(d) { return [d.x, d.y];});
 
-    var rad = this.radius;
     var svg = pane.append("svg")
         .attr("width", width)
         .attr("height", height)
@@ -81,10 +58,9 @@ ReingoldTree.prototype.draw = function(pane, width, height, clickFunction, hover
 
     var svgGroup = svg.append('g');
 
-
     // List of edges
     var link = svgGroup.selectAll('link')
-        .data(this.links)
+        .data(links)
         .enter()
         .append('g')
         .attr('fill', 'none')
@@ -93,15 +69,13 @@ ReingoldTree.prototype.draw = function(pane, width, height, clickFunction, hover
         .attr('stroke', '#666')
         .attr('d', diagonal);
 
-    var focus = this.focus;
-
     var circles = svgGroup.selectAll("circle")
-        .data(this.nodes)
+        .data(nodes)
         .enter()
         .append('circle')
         .attr('class', 'node')
         .attr('fill', function(d) { return d == focus ? '#fd6500' : d.children ? color(d.children.length) : 'white'; })
-        .attr('r', this.radius)
+        .attr('r', 15)
         .attr('transform', function(d){ return "translate("+d.x+","+d.y+")";})
         .style('cursor', 'pointer')
         .style('stroke', function(d) { return d.children ? color(d.children.length) : '000';})
@@ -110,8 +84,6 @@ ReingoldTree.prototype.draw = function(pane, width, height, clickFunction, hover
         .on('mouseover', function(d) { if ( hoverFunction !== undefined) hoverFunction(d, 0) })
         .on('mouseleave', function(d) { if ( hoverFunction !== undefined) hoverFunction(focus, 1) });
 
-    centerNode(this.focus);
-    if (clickFunction != undefined ) clickFunction(this.focus);
-
-
+    centerNode(focus);
+    if (clickFunction != undefined ) clickFunction(focus);
 }
