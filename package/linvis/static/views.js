eeb9292b06f7a52b8ea5636891e09d6a59f5a39f
Evan Wilde <etcwilde@uvic.ca>
2017-05-22 14:45:49 -0700

    Adding nested details table to author data
    The nested table provides more information about what contributions the
    author made to a given merge. It lists out the files, which commit the
    file is attributed to, and how many lines were added and removed from a
    file in a given commit. Furthermore, it provides a link directly to that
    commit.

--- package/linvis/static/views.js
+++ package/linvis/static/views.js
@@ -3,6 +3,8 @@
  * May 14, 2017
  */
 
+
+// Authorship processor
 var drawAuthors = function(authors, pane) {
     let data = $.map(authors, function(value, index) {return [value];});
     let tab = $("<table></table>",
@@ -10,16 +12,63 @@ var drawAuthors = function(authors, pane) {
          "class": "display table table-striped table-bordered",
          "width": "100%"});
     pane.html(tab);
-    tab.DataTable({
+
+    let dataTable = tab.DataTable({
         data: data,
+        order: [[1, 'desc']],
         columns: [
+            {orderable: false, className: 'details-control',  data: null, defaultContent: '<span class=\'glyphicon glyphicon-menu-right\'></span>'},
             {title: 'Author', data: 'author'},
+            {title: 'Files', data: 'files.length'},
             {title: 'Added', data: 'added'},
             {title: 'Removed', data: 'removed'}
         ]
     });
+
+    var authorDetails = function(row, d) {
+        let tab = $('<table></table>',
+            {   'id': 'nested-table',
+                'class': 'display table table-striped table-bordered',
+                'width': '100%'});
+
+        let header = $('<thead></thead>').append(
+            $('<tr></tr>')
+            .append($('<td></td>').html('Commit'))
+            .append($('<td></td>').html('Filename'))
+            .append($('<td></td>').html('Lines Added'))
+            .append($('<td></td>').html('Lines Removed')));
+
+        let body = $('<tbody></tbody>');
+
+        for (var f in d.files) {
+            let data = d.files[f];
+            body.append($('<tr></tr>')
+                .append($('<td></td>').html($('<a></a>', {'href': '/commits/' + data.cid})
+                    .html(data.cid.substring(0, 10))))
+                .append($('<td></td>').html(data.fname))
+                .append($('<td></td>').html(data.added))
+                .append($('<td></td>').html(data.removed)));
+        }
+        return tab.append(header).append(body);
+    }
+
+    tab.on('click', 'td.details-control', function() {
+        let tr = $(this).closest('tr')
+            row = dataTable.row(tr);
+        if (row.child.isShown()) {
+            row.child.hide();
+            $(this).html('<span class=\'glyphicon glyphicon-menu-right\'></span');
+        } else {
+            row.child(authorDetails(tr, row.data())).show();
+            $(this).html('<span class=\'glyphicon glyphicon-menu-down\'></span');
+            $(this).parent().next().children().children().DataTable({});
+        }
+    });
+
 }
 
+
+// File Data processor
 var drawFiles = function(files, pane) {
     let data = $.map(files, function(value, index) { return [value];});
     let tab = $("<table></table>", { "id": "file-table",
@@ -36,6 +85,8 @@ var drawFiles = function(files, pane) {
     });
 }
 
+
+// Module pprocessor
 var drawModules = function(modules, pane) {
     let data = $.map(modules, function(value, index) { return [value];});
     let tab = $("<table></table>", {"id": "module-table",
